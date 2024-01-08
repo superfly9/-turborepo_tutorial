5afb70f1d74607b15f341c761d366cb6cd5b2672
송승찬 <superfly09@daum.net>
2024-01-09 01:22:26 +0900

    fix: routes group 추가 - 홈의 레이아웃과 검색 페이지의 레이아웃이 달라야 하는 상황 - Root Layout이 중첩되지 않도록 route group으로 분리 - 각 ( ) group에 layout.tsx 추가 / 여기에 html 과 body 태그가 필수

--- apps/web/app/(home)/page.tsx
+++ apps/web/app/(home)/page.tsx
@@ -1,7 +1,7 @@
 import Feed from "component/Feed";
 import Story from "component/Story";
 import styles from "./page.module.css";
-import ModalProvider from "./context/ModalContext/Provider";
+import ModalProvider from "../context/ModalContext/Provider";
 
 export default function Page(): JSX.Element {
   return (
